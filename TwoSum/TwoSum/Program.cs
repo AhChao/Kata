62e5c26c18bcafed47b2c6219acd775ad95ecf4b
Steven Chao <gash612@gmail.com>
2019-05-09 19:34:36 +0800

    TwoSum Kata upload

---
+++ TwoSum/TwoSum/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwoSum
+{
+    public class Solution
+    {
+        static void Main(string[] args)
+        {
+        }
+        public static bool ArrayCmp(int []arr1,int []arr2)
+        {
+            if (arr1.Length != arr2.Length) return false;
+            for (int i = 0; i < arr1.Length; i++)
+            {
+                if (arr1[i] != arr2[i]) return false;
+            }
+            return true;
+        }
+
+        public static int[] TwoSum(int[] nums, int target)
+        {
+            int[] minusNums = nums;
+            int[] answer = new int[2];
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = 0; j < minusNums.Length; j++)
+                {
+                    if (j >= i) break;
+                    if (minusNums[j] == nums[i])
+                    {
+                        answer[0] = j;
+                        answer[1] = i;
+                        return answer;
+                    }
+                }
+                minusNums[i] = target - nums[i];
+            }
+            return nums;
+        }
+    }
+}
---
+++ TwoSum/TwoSumTests/UnitTest1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TwoSum;
+
+namespace TwoSumTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void arrayCmpTest()
+        {
+            int[] arr1 = { 2, 4 };
+            int[] arr2 = { 2, 4 };
+            Assert.IsTrue(Solution.ArrayCmp(arr1, arr2),"Wrong Case");
+        }
+        [TestMethod]
+        public void arrayCmpErrTest()
+        {
+            int[] arr1 = { 2, 4 };
+            int[] arr2 = { 2, 5 };
+            Assert.IsFalse(Solution.ArrayCmp(arr1, arr2), "Wrong Case");
+        }
+        [TestMethod]
+        public void twoSumTest()
+        {
+            int [] nums = {2,7,11,15};
+            int target = 9;
+            int [] ans = { 0, 1 };
+            Assert.IsTrue(Solution.ArrayCmp(Solution.TwoSum(nums, target),ans), "Wrong Case");
+        }
+    }
+}
