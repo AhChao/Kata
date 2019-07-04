757c3447889f7a631a4ab8ebaf214fabfd815ed9
Steven Chao <gash612@gmail.com>
2019-07-04 21:58:52 +0800

    StartTDD LA

---
+++ TennisKata/C#TDD/TennisKata_TDD/TennisKata_TDD/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TennisKata_TDD")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TennisKata_TDD")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c70daf5a-d42b-487d-b149-ff200b4bc908")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TennisKata/C#TDD/TennisKata_TDD/TennisKata_TDD/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TennisKata_TDD
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Score_0_0_ShouldBe_Love_All()
+        {
+            Assert.AreEqual(ScoreMsg(),"Love All");
+        }
+    }
+}
