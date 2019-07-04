ae1048c7572d574af234e98f3451e51f8d209a0d
Steven Chao <gash612@gmail.com>
2019-07-04 22:10:25 +0800

    Refactor

--- TennisKata/C#TDD/TennisKata_TDD/TennisKata_TDD/UnitTest1.cs
+++ TennisKata/C#TDD/TennisKata_TDD/TennisKata_TDD/UnitTest1.cs
@@ -6,17 +6,17 @@ namespace TennisKata_TDD
     [TestClass]
     public class UnitTest1
     {
+        TennisCounter counter = new TennisCounter();
+
         [TestMethod]
         public void Score_0_0_ShouldBe_Love_All()
         {
-            TennisCounter counter = new TennisCounter();
             Assert.AreEqual(counter.ScoreMsg(),"Love All");
         }
 
         [TestMethod]
         public void Score_1_0_ShouldBe_Fifteen_Love()
         {
-            TennisCounter counter = new TennisCounter();
             counter.FirstPlayerScore();
             Assert.AreEqual(counter.ScoreMsg(), "Fifteen_Love");
         }
