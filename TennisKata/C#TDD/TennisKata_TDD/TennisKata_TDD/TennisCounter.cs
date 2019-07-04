ad7287de19ed395876ebf81c78d4274ccb1cb784
Steven Chao <gash612@gmail.com>
2019-07-04 22:29:16 +0800

    Pass 0_1 ShouldBe Love_Fifteen

--- TennisKata/C#TDD/TennisKata_TDD/TennisKata_TDD/TennisCounter.cs
+++ TennisKata/C#TDD/TennisKata_TDD/TennisKata_TDD/TennisCounter.cs
@@ -9,24 +9,29 @@ namespace TennisKata_TDD
     class TennisCounter
     {
         private int _firstPlayerScore;
+        private int _secondPlayerScore;
 
         public string ScoreMsg()
         {
             Dictionary<int,string> ScoreString = new Dictionary<int,string>()
             {
+                {0,"Love"},
                 {1,"Fifteen"},
                 {2,"Thirty"},
                 {3,"Forty"},
             };
+            if (_firstPlayerScore == 0 && _secondPlayerScore == 0) return "Love All";
             if (_firstPlayerScore == 4) return "Player1 Won";
-            if (_firstPlayerScore >= 1) return ScoreString[_firstPlayerScore] + " Love";
-
-            return "Love All";
+             return ScoreString[_firstPlayerScore] + " " + ScoreString[_secondPlayerScore];    
         }
 
         public void FirstPlayerScore(int score)
         {
             _firstPlayerScore += score;
+        }
+        public void SecondPlayerScore(int score)
+        {
+            _secondPlayerScore += score;
         }
     }
 }
